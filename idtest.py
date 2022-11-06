2f806fa7858185213cfa972f65e561de1eb3f2e9
TeunMeijer <113886468+TeunMeijer@users.noreply.github.com>
2022-11-06 20:16:06 +0100

    nieuw

--- idtest.py
+++ idtest.py
@@ -10,15 +10,3 @@ del eenberichtje[1]
 eebericht = open("berichten.csv", 'w')
 eebericht.write("\n".join(eenberichtje))
 
-
-#with open("berichten.csv", "r") as f:
-    #data = f.read().split("\n")
-
-# Remove the 1st line
-#del data[1]
-
-# Save the data
-#with open("berichten.csv", "w") as f:
-   # f.write("\n".join(data))
-
-
